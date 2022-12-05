84752e68c167d7ff52e3c2a9e8374f07873afb85
Andy Cogbill <acogbill@twilio.com>
2022-12-05 16:48:04 -0500

    Initial Commit

---
+++ src/routes/posts.js
@@ -0,0 +1,10 @@
+// /routes/posts.js
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  //res.send('You have hit GET /posts endpoint')
+  res.send({response: 'You have hit GET / posts endpoint'})
+})
+
+module.exports = router
