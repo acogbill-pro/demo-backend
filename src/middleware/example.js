0bc1384770adb65ed0740712ad0a2e09cafbed45
Andy Cogbill <acogbill@twilio.com>
2022-12-05 17:25:30 -0500

    Adding in a middleware test

---
+++ src/middleware/example.js
@@ -0,0 +1,6 @@
+// /routes/posts.js
+const express = require('express')
+
+const analytics = 'You have hit GET / posts endpoint loser'
+
+module.exports = analytics
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,10 +1,11 @@
 // /routes/posts.js
 const express = require('express')
+const string = require('../middleware/example.js')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
   //res.send('You have hit GET /posts endpoint')
-  res.send({response: 'You have hit GET / posts endpoint'})
+  res.send({response: string})
 })
 
 module.exports = router
